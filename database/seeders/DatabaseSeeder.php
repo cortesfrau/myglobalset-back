5e2aedff8180c7ae3ca20c2342e2f81125f26468
Lluís Cortès Frau <lluiscortes@uoc.edu>
2023-12-12 02:40:29 +0100

    Database work

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'John',
+            'last_name' => 'Smith',
+            'email' => 'john@smith.com',
         ]);
     }
 }
