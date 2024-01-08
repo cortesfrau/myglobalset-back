58a1aa829d4561c325175d66a8ec117e69218291
Lluís Cortès Frau <lluiscortes@uoc.edu>
2024-01-08 19:15:56 +0100

    Added set_icon to print model

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Collection;
 use Illuminate\Database\QueryException;
@@ -13,58 +14,83 @@ use App\Http\Controllers\ScryfallController;
 
 class CollectionController extends Controller
 {
-  public function create(Request $request)
-  {
-    try {
+    /**
+     * Create a new collection for a user.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        try {
+            // Validate the incoming request data
+            $request->validate([
+                'user_id' => 'required|exists:users,id',
+                'card_id' => 'required',
+                'card_name' => 'required',
+            ]);
 
-      $request->validate([
-          'user_id' => 'required|exists:users,id',
-          'card_id' => 'required',
-          'card_name' => 'required',
-      ]);
+            // Create a new Collection instance
+            $collection = new Collection([
+                'user_id' => $request->user_id,
+                'card_id' => $request->card_id,
+                'card_name' => $request->card_name,
+            ]);
 
-      $collection = new Collection([
-          'user_id' => $request->user_id,
-          'card_id' => $request->card_id,
-          'card_name' => $request->card_name,
-      ]);
+            // Save the collection to the database
+            $collection->save();
 
-      $collection->save();
+            // Return a successful response
+            return response()->json(['message' => 'Collection created.'], 201);
+        } catch (QueryException $exception) {
+            // Handle unique constraint violation (collection already exists for this card)
+            if ($exception->errorInfo[1] === 1062) {
+                return response()->json(['error' => 'A collection for this card already exists.'], 400);
+            }
 
-      return response()->json(['message' => 'Collection created.'], 201);
-
-    } catch (QueryException $exception) {
-
-      if ($exception->errorInfo[1] === 1062) {
-          return response()->json(['error' => 'A collection for this card already exists.'], 400);
-      }
-
-      throw $exception;
+            // Re-throw other exceptions
+            throw $exception;
+        }
     }
-  }
 
-  public function getUserCollections($userId)
-  {
-      try {
-          $userExists = User::where('id', $userId)->exists();
+    /**
+     * Get all collections for a specific user.
+     *
+     * @param int $userId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getUserCollections($userId)
+    {
+        try {
+            // Check if the user exists
+            $userExists = User::where('id', $userId)->exists();
 
-          if (!$userExists) {
-              return response()->json(['error' => 'User not found.'], 404);
-          }
+            if (!$userExists) {
+                return response()->json(['error' => 'User not found.'], 404);
+            }
 
-          $collections = Collection::where('user_id', $userId)->get();
+            // Get collections for the specified user
+            $collections = Collection::where('user_id', $userId)->get();
 
-          if ($collections->isEmpty()) {
-              return response()->json(['error' => 'No collections found for the user.'], 404);
-          }
+            if ($collections->isEmpty()) {
+                return response()->json(['error' => 'No collections found for the user.'], 404);
+            }
 
-          return response()->json(['collections' => $collections], 200);
+            // Return collections as a JSON response
+            return response()->json(['collections' => $collections], 200);
 
-      } catch (\Exception $exception) {
-          return response()->json(['error' => 'Error retrieving user collections.'], 500);
-      }
-  }
+        } catch (\Exception $exception) {
+            // Handle general exceptions
+            return response()->json(['error' => 'Error retrieving user collections.'], 500);
+        }
+    }
 
+    /**
+     * Delete a collection and associated collected card prints.
+     *
+     * @param int $collectionId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function delete($collectionId)
     {
         try {
@@ -90,62 +116,74 @@ class CollectionController extends Controller
         }
     }
 
+    /**
+     * Get details of a specific collection.
+     *
+     * @param int $collectionId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getCollection($collectionId)
     {
         try {
-
+            // Check if the collection exists
             $collectionExists = Collection::where('id', $collectionId)->exists();
 
             if (!$collectionExists) {
                 return response()->json(['error' => 'Collection not found.'], 404);
             }
 
+            // Get the collection details
             $collection = Collection::where('id', $collectionId)->get();
 
+            // Return the collection details as a JSON response
             return response()->json($collection[0], 200);
 
         } catch (\Exception $exception) {
-
+            // Handle exceptions during the process
             return response()->json(['error' => 'Error retrieving collection.'], 500);
         }
     }
 
+    /**
+     * Get the content of a specific collection, including card prints and their collection status.
+     *
+     * @param int $collectionId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getCollectionContent($collectionId)
     {
         try {
-            // Verificar si la colección existe
+            // Check if the collection exists
             $collectionExists = Collection::where('id', $collectionId)->exists();
 
             if (!$collectionExists) {
                 return response()->json(['error' => 'Collection not found.'], 404);
             }
 
-            // Obtener la colección
+            // Get the collection details
             $collection = Collection::find($collectionId);
 
-            // Verificar si la carta existe en Scryfall
+            // Instantiate ScryfallController for fetching card data
             $scryfallController = new ScryfallController();
             $cardData = $scryfallController->getCardByOracleId($collection->card_id);
 
+            // Process card prints and check their collection status
             $cardPrints = $cardData['prints'];
-
             foreach ($cardPrints as &$cardPrint) {
-                // Verificar si la clave 'id' está presente en $cardPrint antes de acceder a ella
                 $scryfallId = isset($cardPrint['id']) ? $cardPrint['id'] : null;
 
-                // Loguear el valor de $scryfallId
-                \Log::info("Valor de \$scryfallId: {$scryfallId}");
+                \Log::info("Value of \$scryfallId: {$scryfallId}");
 
-                // Verificar si el print está en la tabla collected_card_prints
+                // Check if the card print is collected in the specified collection
                 $isCollected = CollectedCardPrint::where('collection_id', $collectionId)
                     ->where('scryfall_id', $scryfallId)
                     ->exists();
 
-                // Añadir la propiedad is_collected a cada impresión
+                // Add the 'is_collected' property to each card print
                 $cardPrint['is_collected'] = $isCollected;
             }
 
-            // Construir el objeto final con la misma arquitectura que getCardPrintingsdByOracleId
+            // Format the response data
             $formattedData = [
                 'name' => $cardData['name'],
                 'oracle_id' => $cardData['oracle_id'],
@@ -153,63 +191,76 @@ class CollectionController extends Controller
                 'prints' => array_values($cardPrints),
             ];
 
+            // Return the formatted data as a JSON response
             return response()->json($formattedData, 200);
 
         } catch (\Exception $exception) {
-            // Loguear la excepción
-            \Log::error("Error en getCollectionContent: {$exception->getMessage()}");
+            // Log the exception
+            \Log::error("Error in getCollectionContent: {$exception->getMessage()}");
             return response()->json(['error' => 'Error retrieving collection content.'], 500);
         }
     }
 
+    /**
+     * Get statistics for a specific collection, including collected and total prints count.
+     *
+     * @param int $collectionId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getCollectionStats($collectionId)
     {
         try {
+            // Check if the collection exists
             $collectionExists = Collection::where('id', $collectionId)->exists();
 
             if (!$collectionExists) {
                 return response()->json(['error' => 'Collection not found.'], 404);
             }
 
+            // Get the count of collected card prints for the specified collection
             $collectedCardPrintCount = DB::table('collected_card_prints')
                 ->where('collection_id', $collectionId)
                 ->count();
 
+            // Get details of the specified collection
             $collection = DB::table('collections')
                 ->where('id', $collectionId)
                 ->first();
 
+            // Check for valid card_id in the collection details
             if (!$collection || !isset($collection->card_id)) {
                 return response()->json(['error' => 'Invalid or missing card_id in the collection.'], 500);
             }
 
             $card_id = $collection->card_id;
 
+            // Fetch total prints for the card from Scryfall
             $scryfallController = new ScryfallController;
             $totalPrintsResponse = $scryfallController->getCardByOracleId($card_id);
 
+            // Check if the response is valid and contains prints
             if ($totalPrintsResponse && isset($totalPrintsResponse['prints'])) {
                 $totalPrints = $totalPrintsResponse['prints'];
             } else {
                 return response()->json(['error' => 'Error retrieving total card print count from Scryfall.'], 500);
             }
 
+            // Calculate completed percentage based on collected and total prints count
             $completedPercentage = round($collectedCardPrintCount * 100 / count($totalPrints), 1);
 
+            // Prepare the response data
             $data = [
                 "collected_prints_count" => $collectedCardPrintCount,
                 "total_prints_count" => count($totalPrints),
                 "completed_percentage" => $completedPercentage,
             ];
 
+            // Return the response data as a JSON response
             return response()->json($data, 200);
 
         } catch (\Exception $exception) {
+            // Return an error response for any exception during the process
             return response()->json(['error' => 'Error retrieving collection stats.', 'message' => $exception->getMessage()], 500);
         }
     }
-
-
-
-
 }
--- app/Http/Controllers/ScryfallController.php
+++ app/Http/Controllers/ScryfallController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Http;
 class ScryfallController extends Controller
 {
 
+    /**
+     * Get card details by Oracle ID.
+     *
+     * @param string $oracleId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getCardByOracleId($oracleId)
     {
         try {
@@ -31,6 +37,12 @@ class ScryfallController extends Controller
         }
     }
 
+    /**
+     * Map card printings for response.
+     *
+     * @param array $cardPrintings
+     * @return array
+     */
     private function mapCardPrintings($cardPrintings)
     {
         $languagesData = config('languages');
@@ -41,63 +53,78 @@ class ScryfallController extends Controller
             if (isset($languagesData[$languageCode])) {
                 $languageData = $languagesData[$languageCode];
 
+                $setIcon = $this->getSetIcon($cardPrinting['set_id']);
+
                 return [
-                'id' => $cardPrinting['id'],
-                'set_name' => $cardPrinting['set_name'],
-                'set_id' => $cardPrinting['set_id'],
-                'lang' => [
-                    'name' => $languageData['name'],
-                    'code' => $languageData['code'],
-                    'flag_icon' => $languageData['flag_icon'],
-                ],
-                'image_uri' => $cardPrinting['image_uris']['png'],
-                'digital' => $cardPrinting['digital'],
-                'artist' => $cardPrinting['artist'],
-                'set_release_date' => $cardPrinting['released_at'],
-                'foil' => $cardPrinting['foil'],
-                'nonfoil' => $cardPrinting['nonfoil'],
-                'is_collected' => null
+                    'id' => $cardPrinting['id'],
+                    'set_name' => $cardPrinting['set_name'],
+                    'set_id' => $cardPrinting['set_id'],
+                    'lang' => [
+                        'name' => $languageData['name'],
+                        'code' => $languageData['code'],
+                        'flag_icon' => $languageData['flag_icon'],
+                    ],
+                    'image_uri' => $cardPrinting['image_uris']['png'],
+                    'digital' => $cardPrinting['digital'],
+                    'artist' => $cardPrinting['artist'],
+                    'set_release_date' => $cardPrinting['released_at'],
+                    'foil' => $cardPrinting['foil'],
+                    'nonfoil' => $cardPrinting['nonfoil'],
+                    'is_collected' => null,
+                    'set_icon' => $setIcon,
+                ];
+            } else {
+                $english = [
+                    'name' => 'English',
+                    'code' => 'en',
+                    'flag_icon' => 'fi fi-gb fis',
                 ];
 
-            } else {
-
-            $english = [
-                'name' => 'English',
-                'code' => 'en',
-                'flag_icon' => 'fi fi-gb fis',
-            ];
-
-            return [
-                'id' => $cardPrinting['oracle_id'],
-                'set_name' => $cardPrinting['set_name'],
-                'set_id' => $cardPrinting['set_id'],
-                'lang' => $english,
-                'image_uri' => $cardPrinting['image_uris']['png'],
-                'digital' => $cardPrinting['digital'],
-                'artist' => $cardPrinting['artist'],
-                'set_release_date' => $cardPrinting['released_at'],
-            ];
+                return [
+                    'id' => $cardPrinting['oracle_id'],
+                    'set_name' => $cardPrinting['set_name'],
+                    'set_id' => $cardPrinting['set_id'],
+                    'lang' => $english,
+                    'image_uri' => $cardPrinting['image_uris']['png'],
+                    'digital' => $cardPrinting['digital'],
+                    'artist' => $cardPrinting['artist'],
+                    'set_release_date' => $cardPrinting['released_at'],
+                ];
             }
         }, $cardPrintings);
     }
 
+    /**
+     * Get card printings by Oracle ID.
+     *
+     * @param string $oracleId
+     * @return array
+     * @throws \Exception
+     */
     private function getCardPrintingsdByOracleId($oracleId)
     {
         $apiUrl = "https://api.scryfall.com/cards/search?q=oracleid:{$oracleId}&unique=prints&order=released&dir=asc&include_multilingual=true";
 
         try {
-        $response = Http::get($apiUrl);
+            $response = Http::get($apiUrl);
 
-        if ($response->successful()) {
-            return $response->json();
-        } else {
-            throw new \Exception('Error in the Scryfall request');
-        }
+            if ($response->successful()) {
+                return $response->json();
+            } else {
+                throw new \Exception('Error in the Scryfall request');
+            }
         } catch (\Exception $e) {
             throw new \Exception('Error in the Scryfall request: ' . $e->getMessage());
         }
     }
 
+    /**
+     * Get set details by set ID.
+     *
+     * @param string $setId
+     * @return array
+     * @throws \Exception
+     */
     public function getSetById($setId)
     {
         $apiUrl = "https://api.scryfall.com/sets/{$setId}";
@@ -112,6 +139,24 @@ class ScryfallController extends Controller
             }
         } catch (\Exception $e) {
             throw new \Exception('Error in the Scryfall request: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Get set icon URI by set ID.
+     *
+     * @param string $setId
+     * @return string|null
+     */
+    private function getSetIcon($setId)
+    {
+        try {
+            $setInfo = $this->getSetById($setId);
+            $setIcon = isset($setInfo['icon_svg_uri']) ? $setInfo['icon_svg_uri'] : null;
+            return $setIcon;
+        } catch (\Exception $e) {
+            \Log::error("Error fetching set icon: {$e->getMessage()}");
+            return null;
         }
     }
 }
